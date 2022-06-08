adac192e7e5b20ac018f0224b87220ac22e8f929
Mustafa Kaan <ozmustafakaan@gmail.com>
2022-06-08 15:26:50 +0300

    Payment Form & Stripe and Review | Done

--- src/App.js
+++ src/App.js
@@ -15,6 +15,8 @@ import {
 const App = () => {
   const [products, setProducts] = useState([]);
   const [cart, setCart] = useState({});
+  const [order, setOrder] = useState({});
+  const [errorMessage, setErrorMessage] = useState('');
 
   const fetchProducts = async () => {
     const { data } = await commerce.products.list();
@@ -46,6 +48,23 @@ const App = () => {
     setCart(cart);
   }
 
+  const refreshCart = async () => {
+    const newCart = await commerce.cart.refresh();
+
+    setCart(newCart);
+  }
+
+  const handleCaptureCheckout = async (checkoutTokenId, newOrder) => {
+    try {
+      const incomingOrder = await commerce.checkout.capture(checkoutTokenId, newOrder);
+
+      setOrder(incomingOrder);
+      refreshCart();
+    } catch (error) {
+      setErrorMessage(error.data.error.message);
+    }
+  }
+
 
   useEffect(() => {
     fetchProducts();
@@ -65,7 +84,11 @@ const App = () => {
             handleRemoveFromCart={handleRemoveFromCart}
             handleEmptyCart={handleEmptyCart}
           />} />
-          <Route exact path="/checkout" element={<Checkout cart={cart}
+          <Route exact path="/checkout" element={<Checkout
+            cart={cart}
+            order={order}
+            onCaptureCheckout={handleCaptureCheckout}
+            error={errorMessage}
           />} />
         </Routes>
       </div>
